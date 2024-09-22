b662c3f6eee4543698ac35043559db1d248b8cb1
haitrth <haithanhtran00@gmail.com>
2024-09-22 19:27:29 +0700

    JSX

---
+++ Create ReactJS/first_project/src/test.jsx
@@ -0,0 +1,27 @@
+function MyButton() {
+  return (
+    <button> I'm a button</button>
+  )
+}
+export default function myApp(){
+  return (
+    <>
+        <h1>Welcome to my app</h1>
+        <h1>About</h1>
+        <p>Hello there. <br /> How do you do?</p>
+        <img className="avatar"></img>
+        <MyButton/>
+    </>
+  )
+  return (
+    <h1>
+      {user.name}
+    </h1>
+  )
+  return (
+    <img
+    className="avatar"
+    src={user.imagUrl}>
+    </img>
+  )
+} 
